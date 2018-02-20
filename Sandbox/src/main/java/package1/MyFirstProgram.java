3cfe86d37de31ab6d9aeab47474ea1e6e676dad5
Hanna <jezierskahanna88@gmail.com>
2018-02-20 20:49:11 +0100

    Added new func

--- Sandbox/src/main/java/package1/MyFirstProgram.java
+++ Sandbox/src/main/java/package1/MyFirstProgram.java
@@ -3,6 +3,27 @@ package package1;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        hello("world");
+        hello ("Hana");
+
+        double len = 5;
+        System.out.println("Powierz boku " + len + " = " + area(len));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Pow prostokata o bokach " + a + " i " +  b  + " = " + area(a, b));
+
+    }
+
+    public static void hello(String somebody){
+
+        System.out.println("Hello," + somebody + "!");
+    }
+    public static double area(double len){
+        return len * len;
+    }
+
+    public static double area(double a, double b){
+        return a * b;
     }
 }
