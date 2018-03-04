d621c0de096d437fe51c9b457d89dc014cc525cb
Hanna <jezierskahanna88@gmail.com>
2018-03-04 21:18:38 +0100

    if then else lesson 2

--- Sandbox/src/main/java/nu/stqa/pft/sandbox/Equation.java
+++ Sandbox/src/main/java/nu/stqa/pft/sandbox/Equation.java
@@ -7,27 +7,37 @@ public class Equation {
   private double c;
   private int n;
 
-  public Equation (double a, double b, double c){
+  public Equation(double a, double b, double c) {
 
 
     this.a = a;
     this.b = b;
     this.c = c;
 
-    double d = b*b - 4*a*c;
+    double d = b * b - 4 * a * c;
 
-    if(d > 0){
-      n = 2;
-    }else{
-      if (d == 0){
+    if (a != 0) {
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
         n = 1;
-      }else {
+      } else {
         n = 0;
       }
-    }
-  }
 
-  public int rootNumber(){
+
+    } else if (b != 0) {
+      n = 1;
+
+    } else if (c != 0) {
+      n = 0;
+
+    } else {
+      n =  -1;
+    }
+
+  }
+  public int rootNumber() {
     return 0;
   }
 }
