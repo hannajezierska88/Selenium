248cb14440c9b99fe0bd17dd9f1932c2334f401c
Hanna <jezierskahanna88@gmail.com>
2018-03-04 20:54:01 +0100

    If Else

---
+++ Sandbox/src/test/java/nu/stqa/pft/sandbox/EquationTest.java
@@ -0,0 +1,26 @@
+package nu.stqa.pft.sandbox;
+
+        import org.testng.Assert;
+        import org.testng.annotations.Test;
+
+public class EquationTest {
+
+  @Test
+  public void test0(){
+    Equation e = new Equation (1, 1, 1);
+    Assert.assertEquals(e.rootNumber(), 0);
+
+  }
+
+  public void test1(){
+    Equation e = new Equation (1, 2, 1);
+    Assert.assertEquals(e.rootNumber(), 1);
+
+  }
+
+  public void tes2(){
+    Equation e = new Equation (1, 5, 6);
+    Assert.assertEquals(e.rootNumber(), 2);
+
+  }
+}
