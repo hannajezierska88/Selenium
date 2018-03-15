1069ed16a3f5542a543b6ea9bcd440734f106b31
Hanna <jezierskahanna88@gmail.com>
2018-03-15 20:42:35 +0100

    Select specific element

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTest.java
@@ -14,7 +14,7 @@ public class GroupModificationTest extends TestBase {
     if ( ! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before -1 );
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Test1", "Testheader", "Testhadertest"));
     app.getGroupHelper().sumbitGroupModification();
